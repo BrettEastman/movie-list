44329f54b6e95f8f9412426a3ce3bad7c88d3bda
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-13 21:09:14 -0800

    Backend working with front end with axios. Full stack achieved!

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import React, { Component } from "react";
 import MovieList from './MovieList.jsx';
 import MovieListItem from './MovieListItem.jsx';
 import Search from './Search.jsx';
 import AddInput from './AddInput.jsx';
+import axios from 'axios';
 
-const App = () => {
-  const [query, setQuery] = React.useState('');
-  const [movies, setMovies] = React.useState([
-    {title: 'Mean Girls', id: 1},
-    {title: 'Hackers', id: 2},
-    {title: 'The Grey', id: 3},
-    {title: 'Sunshine', id: 4},
-    {title: 'Ex Machina', id: 5},
-  ]);
+export const App = () => { // example of named import
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios.get('/movies')
+      .then(({ data }) => {
+        setMovies(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   // write function to add movie with id first
   // stack?
-  // pass function down - as far down as needed, 
+  // pass function down - as far down as needed,
   // useEffect - will run once component has been mounted takes in cb and dependency - tells react when to re-run
 
   let filtered = [...movies].filter((movie) => {
@@ -42,4 +46,4 @@ const App = () => {
   );
 };
 
-export default App;
+// no default due to changing to named import
